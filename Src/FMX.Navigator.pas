64e6187ad54ad3a0655773075595188a54e25af3
Rodrigo Bernardi <coloradonanet@unochapeco.edu.br>
2019-05-29 18:14:45 -0300

    Changed to use a frame base

--- Src/FMX.Navigator.pas
+++ Src/FMX.Navigator.pas
@@ -5,31 +5,35 @@ interface
 uses
   System.SysUtils, System.Classes, System.Generics.Collections, FMX.Types, FMX.Controls, FMX.Layouts, FMX.StdCtrls,
   FMX.Objects, FMX.Graphics, FMX.MultiView, FMX.Effects, System.UITypes,
-  FMX.Forms;
+  FMX.Forms, FMX.Controls.Presentation;
 
 type
 
-  TNavigator = class(TLayout)
+  TNavigator = class(TFrame)
+    LayoutHeader: TLayout;
+    ToolBar: TToolBar;
+    LabelCaption: TLabel;
+    ShadowEffectToolbar: TShadowEffect;
+    MenuButton: TButton;
+    BackButton: TButton;
+    procedure BackButtonClick(Sender: TObject);
+    procedure MenuButtonClick(Sender: TObject);
   private
-    FRectangle: TRectangle;
     FMultiView: TMultiView;
     FMenuButton: TButton;
     FBackButton: TButton;
     FMultiViewButton: TButton;
     FStack: TStack<TPair<string, TFrame>>;
-    FTitle: TLabel;
     FFontColor: TAlphaColor;
     procedure SetMultiView(const Value: TMultiView);
-    procedure DoOnClickMultiViewButton(Sender: TObject);
-    procedure DoOnClickBackButton(Sender: TObject);
     function HasMultiView: Boolean;
     function StackIsEmpty: Boolean;
-    procedure SetFill(const Value: TBrush);
-    function GetFill: TBrush;
     function GetTitle: string;
     procedure SetTitle(const Value: string);
     procedure SetFontColor(const Value: TAlphaColor);
     procedure DoPush(TitleNavigator: string; Frame: TFrame);
+    function GetTintColor: TAlphaColor;
+    procedure SetTintColor(const Value: TAlphaColor);
   protected
     procedure Notification(AComponent: TComponent; Operation: TOperation); override;
   public
@@ -39,9 +43,10 @@ type
     procedure Push(Frame: TFrame); overload;
     procedure Push(NavigatorTitle: string; Frame: TFrame); overload;
     procedure Pop;
+
   published
     property MultiView: TMultiView read FMultiView write SetMultiView;
-    property Fill: TBrush read GetFill write SetFill;
+    property TintColor: TAlphaColor read GetTintColor write SetTintColor;
     property Title: string read GetTitle write SetTitle;
     property FontColor: TAlphaColor read FFontColor write SetFontColor default TAlphaColorRec.Black;
   end;
@@ -50,6 +55,8 @@ procedure Register;
 
 implementation
 
+{$R *.fmx}
+
 procedure Register;
 begin
   RegisterComponents('HashLoad', [TNavigator]);
@@ -57,45 +64,25 @@ end;
 
 { TNavigator }
 
+procedure TNavigator.BackButtonClick(Sender: TObject);
+begin
+  Pop;
+end;
+
 constructor TNavigator.Create(AOwner: TComponent);
 begin
   inherited;
 
   FStack := TStack<TPair<string, TFrame>>.Create;
+  LayoutHeader.SetSubComponent(True);
+  ToolBar.SetSubComponent(True);
+  ShadowEffectToolbar.SetSubComponent(True);
+  LabelCaption.SetSubComponent(True);
+  BackButton.SetSubComponent(True);
+  MenuButton.SetSubComponent(True);
 
   Align := TAlignLayout.Top;
   Height := 56;
-
-  FRectangle := TRectangle.Create(Self);
-  FRectangle.SetSubComponent(True);
-  FRectangle.Stored := False;
-  FRectangle.Stroke.Kind := TBrushKind.None;
-  FRectangle.Align := TAlignLayout.Client;
-  FRectangle.Parent := Self;
-
-  FMenuButton := TButton.Create(FRectangle);
-  FMenuButton.SetSubComponent(True);
-  FMenuButton.Stored := False;
-  FMenuButton.StyleLookup := 'drawertoolbutton';
-  FMenuButton.Align := TAlignLayout.Left;
-  FMenuButton.Parent := FRectangle;
-  FMenuButton.OnClick := DoOnClickMultiViewButton;
-
-  FBackButton := TButton.Create(FRectangle);
-  FBackButton.SetSubComponent(True);
-  FBackButton.Stored := False;
-  FBackButton.Visible := False;
-  FBackButton.StyleLookup := 'backtoolbutton';
-  FBackButton.Align := TAlignLayout.Left;
-  FBackButton.Parent := FRectangle;
-  FBackButton.OnClick := DoOnClickBackButton;
-
-  FTitle := TLabel.Create(FRectangle);
-  FTitle.SetSubComponent(True);
-  FTitle.Stored := False;
-  FTitle.Align := TAlignLayout.Client;
-  FTitle.TextSettings.Font.Size := 20;
-  FTitle.Parent := FRectangle;
 
   FontColor := TAlphaColorRec.Black;
 
@@ -112,30 +99,25 @@ begin
   inherited;
 end;
 
-procedure TNavigator.DoOnClickBackButton(Sender: TObject);
+function TNavigator.GetTintColor: TAlphaColor;
 begin
-  Pop;
-end;
-
-procedure TNavigator.DoOnClickMultiViewButton(Sender: TObject);
-begin
-  if Assigned(FMultiView) then
-    FMultiViewButton.OnClick(Sender);
-end;
-
-function TNavigator.GetFill: TBrush;
-begin
-  Result := FRectangle.Fill;
+  Result := ToolBar.TintColor;
 end;
 
 function TNavigator.GetTitle: string;
 begin
-  Result := FTitle.Text;
+  Result := LabelCaption.Text;
 end;
 
 function TNavigator.HasMultiView: Boolean;
 begin
   Result := FMultiView <> nil;
+end;
+
+procedure TNavigator.MenuButtonClick(Sender: TObject);
+begin
+  if Assigned(FMultiView) then
+    FMultiViewButton.OnClick(Sender);
 end;
 
 function TNavigator.StackIsEmpty: Boolean;
@@ -197,17 +179,12 @@ begin
   DoPush(Title, Frame);
 end;
 
-procedure TNavigator.SetFill(const Value: TBrush);
-begin
-  FRectangle.Fill := Value;
-end;
-
 procedure TNavigator.SetFontColor(const Value: TAlphaColor);
 begin
   if FFontColor <> Value then
   begin
     FFontColor := Value;
-    FTitle.TextSettings.FontColor := Value;
+    LabelCaption.TextSettings.FontColor := Value;
     FBackButton.IconTintColor := Value;
     FMenuButton.IconTintColor := Value;
   end;
@@ -227,10 +204,15 @@ if FMultiView <> Value then
   end;
 end;
 
+procedure TNavigator.SetTintColor(const Value: TAlphaColor);
+begin
+  ToolBar.TintColor := Value;
+end;
+
 procedure TNavigator.SetTitle(const Value: string);
 begin
-  if FTitle.Text <> Value then
-    FTitle.Text := Value;
+  if LabelCaption.Text <> Value then
+    LabelCaption.Text := Value;
 end;
 
 end.
